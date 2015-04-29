a986d9fd3526b8493d94e5644006c74001d80a88
mosszhaodphil <mosszhao@Mosss-MacBook-Pro.local>
2015-04-29 20:33:33 +0100

    rename gm masked perfusion file

--- estimate_cbf.sh
+++ estimate_cbf.sh
@@ -4,4 +4,4 @@ fabber --data=asldata_gm_k5 --data-order=singlefile --output=full -@ options.txt
 
 fslmaths full/mean_ftiss.nii.gz -div M0t_gm_k5 -mul 0.9 -mul 6000 perfusion_gm_k5_scaled
 
-fslmaths perfusion_gm_k5_scaled -mas gm_mask perfusion_gm_k5_scaled_mask
+fslmaths perfusion_gm_k5_scaled -mas gm_mask perfusion_gm_mask
