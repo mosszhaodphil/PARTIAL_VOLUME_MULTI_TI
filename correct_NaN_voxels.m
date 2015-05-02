646878ff10d17d3cbbb4398e79569ed40c589855
mosszhaodphil <mosszhao@Mosss-MacBook-Pro.local>
2015-05-02 22:19:46 +0100

    Add dimension check 4D and 3D

--- correct_NaN_voxels.m
+++ correct_NaN_voxels.m
@@ -1,25 +1,48 @@
-
+% This function correct all NaN and infinity voxels of a nifty image to zero
 
 function correct_NaN_voxels(file_name)
 
+	% Load nifty file image matrix
 	file_extension = '.nii.gz';
 	file_handle = load_nii(strcat(file_name, file_extension));
 	ma = file_handle.img;
 
-	[x, y, z] = size(ma);
+	% Determine the dimension of the matrix
+	d = ndims(ma);
 
-	for i = 1 : x
-		for j = 1 : y
-			for k = 1 : z
-				if(isnan(ma(i, j, k)) || isinf(ma(i, j, k)))
-					ma(i, j, k) = 0;
+	% Correct NaN and infinitive voxels
+	if(d == 3)
+		[x, y, z] = size(ma);
+
+		for i = 1 : x
+			for j = 1 : y
+				for k = 1 : z
+					if(isnan(ma(i, j, k)) || isinf(ma(i, j, k)))
+						ma(i, j, k) = 0;
+					end
 				end
 			end
 		end
 	end
 
-	file_handle.img = ma;
+	if(d == 4)
+		[x, y, z, t] = size(ma);
 
+		for i = 1 : x
+			for j = 1 : y
+				for k = 1 : z
+					for l = 1 : t
+						if(isnan(ma(i, j, k, l)) || isinf(ma(i, j, k, l)))
+							ma(i, j, k, l) = 0;
+						end
+					end
+				end
+			end
+		end
+	end
+
+	% Save the corrected matrix
+	file_handle.img = ma;
 	save_nii(file_handle, strcat(file_name, file_extension));
 
 end
